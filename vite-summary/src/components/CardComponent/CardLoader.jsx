eae6afd070b965c45d5ddfd713993370983593eb
Ruslan <g.ruslan.rus@gmail.com>
2023-09-25 14:20:19 +0300

    v15

--- vite-summary/src/components/CardComponent/CardLoader.jsx
+++ vite-summary/src/components/CardComponent/CardLoader.jsx
@@ -8,68 +8,69 @@ function CardLoader() {
   const [loadingError, setLoadingError] = useState(null);
   const batchSize = 8; // Размер пакета для загрузки
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const allCollectionsRef = collection(db2, 'AllCollections');
-        const querySnapshot = await getDocs(allCollectionsRef);
+  const fetchData = async () => {
+    try {
+      const allCollectionsRef = collection(db2, 'AllCollections');
+      const querySnapshot = await getDocs(allCollectionsRef);
 
-        if (querySnapshot.empty) {
-          console.error('В коллекции "AllCollections" нет документов');
-          return;
-        }
+      if (querySnapshot.empty) {
+        console.error('В коллекции "AllCollections" нет документов');
+        return;
+      }
 
-        const collectionNames = [];
+      const collectionNames = [];
+
+      querySnapshot.forEach((doc) => {
+        const collectionName = doc.data().collectionName;
+        collectionNames.push(collectionName);
+      });
+
+      const fetchPromises = collectionNames.map(async (cardName) => {
+        const collectionRef = collection(db2, cardName);
+        const querySnapshot = await getDocs(collectionRef);
+        const cardsData = [];
 
         querySnapshot.forEach((doc) => {
-          const collectionName = doc.data().collectionName;
-          collectionNames.push(collectionName);
+          const cardData = doc.data();
+          cardsData.push(cardData);
         });
 
-        const fetchPromises = collectionNames.map(async (cardName) => {
-          const collectionRef = collection(db2, cardName);
-          const querySnapshot = await getDocs(collectionRef);
-          const cardsData = [];
+        return cardsData;
+      });
 
-          querySnapshot.forEach((doc) => {
-            const cardData = doc.data();
-            cardsData.push(cardData);
-          });
+      const allCardsData = await Promise.all(fetchPromises);
+      const flattenedCardsData = allCardsData.flat();
 
-          return cardsData;
-        });
+      // Получите первую партию карточек (первые 8 карточек)
+      const initialBatch = flattenedCardsData.slice(0, batchSize);
 
-        const allCardsData = await Promise.all(fetchPromises);
-        const flattenedCardsData = allCardsData.flat();
+      setAllCards(initialBatch);
 
-        // Получите первую партию карточек (первые 8 карточек)
-        const initialBatch = flattenedCardsData.slice(0, batchSize);
+      // Отправьте первую партию в компонент CardsProject
+      // (другой код вашего компонента CardsProject может потребоваться)
+      // <CardsProject allCards={initialBatch} />
 
-        setAllCards(initialBatch);
+      // Загрузите оставшиеся карточки и обновите состояние allCards по мере получения новых данных
+      for (let i = batchSize; i < flattenedCardsData.length; i += batchSize) {
+        const nextBatch = flattenedCardsData.slice(i, i + batchSize);
 
-        // Отправьте первую партию в компонент CardsProject
+        // Обновите состояние allCards с новыми данными
+        setAllCards((prevCards) => [...prevCards, ...nextBatch]);
+
+        // Отправьте новую порцию данных в компонент CardsProject
         // (другой код вашего компонента CardsProject может потребоваться)
-        // <CardsProject allCards={initialBatch} />
-
-        // Загрузите оставшиеся карточки и обновите состояние allCards по мере получения новых данных
-        for (let i = batchSize; i < flattenedCardsData.length; i += batchSize) {
-          const nextBatch = flattenedCardsData.slice(i, i + batchSize);
-
-          // Обновите состояние allCards с новыми данными
-          setAllCards((prevCards) => [...prevCards, ...nextBatch]);
-
-          // Отправьте новую порцию данных в компонент CardsProject
-          // (другой код вашего компонента CardsProject может потребоваться)
-          // <CardsProject allCards={[...prevCards, ...nextBatch]} />
-        }
-      } catch (error) {
-        console.error('Ошибка при получении данных о карточке:', error);
-        setLoadingError(error);
+        // <CardsProject allCards={[...prevCards, ...nextBatch]} />
       }
-    };
+    } catch (error) {
+      console.error('Ошибка при получении данных о карточке:', error);
+      setLoadingError(error);
+      setTimeout(fetchData, 10000); // Повторный запрос через 10 секунд в случае ошибки
+    }
+  };
 
+  useEffect(() => {
     fetchData();
-  }, []);
+  }, []); // Запустите запрос при монтировании компонента
 
   if (loadingError) {
     return <div>Ошибка при загрузке карточек. Попробуйте еще раз позже.</div>;
@@ -81,6 +82,7 @@ function CardLoader() {
 
   return (
     <>
+      {/* Отправьте обновленное состояние allCards в компонент CardsProject */}
       <CardsProject allCards={allCards} />
     </>
   );
